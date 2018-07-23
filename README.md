7400777fcb9bf49f392a06740652cc0744207eda
nyanpadpro <nyanpadpro@gmail.com>
2018-07-23 16:00:25 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# react-test
+# これはなに？
+
+Reactの個人的な学習リポジトリ  
+
+
+## 動作サンプル  
+
+none
+
+## 参考
+
+### git/GitHubの使い方  
+- 初めてGitHubリポジトリにpushしたらrejectedエラーになったときの対応メモ  
+https://qiita.com/takanatsu/items/fc89de9bd11148da1438  
+
---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import DataProvider from "./DataProvider";
+import Table from "./Table";
+
+// ボタン追加
+import Button1 from "./Button1"
+
+/*
+const App = () => (
+<DataProvider endpoint="api/lead/" 
+                render={data => <Table data={data} />
+}
+/>
+);
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<App />, wrapper) : null;
+
+*/
+
+const button1 = document.getElementById("button1");
+button1 ? ReactDOM.render(<Button1 />, button1) : null;
