81b8648b8ebc84648f8125faadfa29084064d9a6
nyanpadpro <nyanpadpro@gmail.com>
2018-07-27 12:11:59 +0000

    動いた

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Reactの個人的な学習リポジトリ
 https://nyanpadpro.github.io/react-test/
 
 ## 参考
+- teratailへの質問
+https://teratail.com/questions/138139
 
 ### git/GitHubの使い方  
 - 初めてGitHubリポジトリにpushしたらrejectedエラーになったときの対応メモ  
--- src/components/Mybutton.js
+++ src/components/Mybutton.js
@@ -6,6 +6,11 @@ export default class Mybutton extends React.Component {
     constructor(props) {
         super(props);
         this.state = { count: 0 };
+        this.handleClick = this.handleClick.bind(this);
+    };
+
+    static defaultProps = {
+        classes: {},
     };
 
     handleClick() {
